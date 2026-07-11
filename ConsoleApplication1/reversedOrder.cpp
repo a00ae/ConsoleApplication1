430023e47365712f2dcab3177d2de9f6d3d8da22
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-07-11 22:02:36 +0300

    7. Solve problem number reverse

--- ConsoleApplication1/reversedOrder.cpp
+++ ConsoleApplication1/reversedOrder.cpp
@@ -14,16 +14,46 @@ int redNumberPositive(string message) {
 
 int printDigits(int number)
 {
-	int sum = 0;
+	/************** problem 6 ***************/
+	//int sum = 0;
 	int remainder = 0;
+	/************** problem 7 ***************/
+	int number2 = 0;
 	while (number > 0) {		
 		remainder = number % 10; //Accessing the units digit
 		number = number / 10; //Removing the units digit
-		sum += remainder;
+		//
+		number2 = number2 * 10 + remainder; 
+		/************** problem 5 ***************/
+		//cout << remainder << endl;
+		/************** problem 6 ***************/
+		//sum += remainder;
+		/************** problem 7 ***************/
+		// هذه عملية اللوب توضح لك ماحدث خلف الكواليس
+		// input => 1234 
+		//1234 % 10 = 4 remainder
+		//4 
+		// 1234 / 10 = 123 number
+		//123*
+		// number2 = 0 * 10 0 + 4
+		// 0 * 10 0 + 4
+		// 4
+		// 3 
+		// 12**
+		// 4 * 10 + 3
+		// 43
+		// 2
+		// 1***
+		// 43 * 10 + 2
+		// 432  
 	}
-	return sum;
+	/************** problem 6 ***************/
+	//return sum;
+	/************** problem 7 ***************/
+	return number2;
 }
 void reversedOrder()
 {
-	cout << printDigits(redNumberPositive("Plase a positive number")) ;
+	cout << "\nSum Of Digits = " << printDigits(redNumberPositive("Plase a positive number")) << "\n";
+	//printDigits(redNumberPositive("Plase a positive number"));
 }
